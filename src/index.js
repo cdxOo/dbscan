41c671978c6840bfdfc30363af7f3a67c0920260
Jan Schwalbe <jan.schwalbe@martaflex.de>
2020-03-25 06:59:02 +0100

    added initial implementation with stub test

---
+++ src/index.js
@@ -0,0 +1,87 @@
+module.exports = ({
+    dataset,
+    epsilon,
+    epsilonCompare,
+    minimumPoints,
+    distanceFunction
+}) => {
+    epsilon = epsilon || 1; // aka maxDistance
+    epsilonCompare = epsilonCompare || ((dist, e) => dist < e);
+    minimumPoints = minimumPoints || 2;
+    distanceFunction = distanceFunction || ((a, b) => Math.abs(a - b));
+
+    var visitedIndices = {};
+        isVisited = (i) => ( visitedIndices[i] ),
+        markVisited = (i) => { visitedIndices[i] = true };
+
+    var clusteredIndices = {};
+        isClustered = (i) => ( clusteredIndices[i] ),
+        markClustered = (i) => { clusteredIndices[i] = true };
+
+    var uniqueMerge = (targetArray, sourceArray) => {
+        for (var i = 0; i < sourceArray.length; i += 1) {
+            var item = sourceArray[i];
+            if (targetArray.indexOf(item) < 0) {
+                targetArray.push(item);
+            }
+        }
+    };
+
+    var findNeighbors = (index) => {
+        var neighbors = [];
+        for (var other = 0; other < dataset.length; other += 1) {
+            var distance = distanceFunction(dataset[index], dataset[other]);
+            if (epsilonCompare(distance, epsilon)) {
+                neighbors.push(other);
+            }
+        }
+        return neighbors;
+    };
+
+    var noise = [],
+        addNoise = (i) => noise.push(i); 
+
+    var clusters = [],
+        createCluster = () => ( clusters.push([]) - 1 ),
+        addIndexToCluster = (c, i) => {
+            clusters[c].push(i);
+            markClustered(i);
+        };
+
+    var expandCluster = (c, neighbors) => {
+        for (var i = 0; i < neighbors.length; i += 1) {
+            var neighborIndex = neighbors[i];
+            if (!isVisited(neighborIndex)) {
+                markVisited(neighborIndex);
+                
+                var secondaryNeighbors = findNeighbors(neighborIndex);
+                if (secondaryNeighbors.length >= minimumPoints) {
+                    uniqueMerge(neighbors, secondaryNeighbors);
+                }
+            }
+
+            if (!isClustered(neighborIndex)) {
+                addIndexToCluster(c, neighborIndex);
+            }
+        }
+    };
+
+    dataset.forEach((unused, index) => {
+        if (!isVisited(index)) {
+            markVisited(index);
+
+            var neighbors = findNeighbors(index);
+            if (neighbors.length < minimumPoints) {
+                noise.push(index);
+            }
+            else {
+                var clusterIndex = createCluster();
+                addIndexToCluster(clusterIndex, index);
+                expandCluster(clusterIndex, neighbors);
+            }
+            
+        }
+    });
+    
+    return ({ clusters, noise });
+}
---
+++ src/index.spec.js
@@ -0,0 +1,19 @@
+var expect = require('chai').expect,
+    dbscan = require('./index');
+
+describe('dbscan()', () => {
+
+    it('clusters stuff as expected', () => {
+        var result = dbscan({
+            dataset: [1,2,3,4, 7,8,9,10, 20],
+            epsilonCompare: (dist, e) => (dist <= e)
+        });
+        
+        //console.log(result);
+        expect(result).to.deep.eql({
+            clusters: [[0,1,2,3], [4,5,6,7]],
+            noise: [8]
+        });
+    });
+
+});
